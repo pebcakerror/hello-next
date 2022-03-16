c83796e5ae9d078cb599b6f73fb853556dd7559a
pebcakerror <613574+pebcakerror@users.noreply.github.com>
2022-03-15 21:51:06 -0500

    Add React Helmet and set up Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
+import Helmet from "react-helmet";
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
-    <div class="bg-blue-900 text-gray-100 p-8 h-screen">
-        <div class="mx-auto mb-4">
-            <div class="h-12 flex items-center mb-4 border-b-2">
-                <a href="#">
-                    <h1 class="font-bold text-2xl">
-                        Rock & Roll <span class="font-normal">with <s>Octane</s> Create React App, React Router, and Redux Toolkit</span>
-                    </h1>
-                </a>
-            </div>
-            <div class="flex flex-wrap -mx-2">
-                fubar
-            </div>
-        </div>
-    </div>
+    <Router>
+      <Helmet>
+        <title>Rock &amp; with React</title>
+      </Helmet>
+      <div className="bg-blue-900 text-gray-100 p-8 h-screen">
+          <div className="mx-auto mb-4">
+              <div className="h-12 flex items-center mb-4 border-b-2">
+                <Link to="/">
+                  <h1 className="font-bold text-2xl">
+                      Rock &amp; Roll <span className="font-normal">React</span>
+                  </h1>
+                </Link>
+              </div>
+              <div className="flex flex-wrap -mx-2">
+                  fubar
+              </div>
+          </div>
+      </div>
+    </Router>
   );
 }
 
