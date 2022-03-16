74a0ed675a3b25b58e9adbc52394027ffb4bd579
pebcakerror <613574+pebcakerror@users.noreply.github.com>
2022-03-15 21:22:59 -0500

    Add Tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="bg-blue-900 text-gray-100 p-8 h-screen">
+        <div class="mx-auto mb-4">
+            <div class="h-12 flex items-center mb-4 border-b-2">
+                <a href="#">
+                    <h1 class="font-bold text-2xl">
+                        Rock & Roll <span class="font-normal">with <s>Octane</s> Create React App, React Router, and Redux Toolkit</span>
+                    </h1>
+                </a>
+            </div>
+            <div class="flex flex-wrap -mx-2">
+                fubar
+            </div>
+        </div>
     </div>
   );
 }
