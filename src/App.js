553da7329806d02fc239ebd94628f6b79f3a3a51
pebcakerror <613574+pebcakerror@users.noreply.github.com>
2022-03-15 22:58:52 -0500

    Re-Add setting page titles with Helmet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { Routes, Route } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 import Layout from "./layout/Layout";
 import Bands from "./bands/Bands";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="bands" element={<Bands />} />
-      </Route>
-    </Routes>
+    <>
+      <Helmet>
+        <title>Rock &amp; Roll with React</title>
+      </Helmet>
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="bands" element={<Bands />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
